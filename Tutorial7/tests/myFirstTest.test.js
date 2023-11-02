1c2ba39e1ee3585d2468dbd337a1f27068910d68
Vithu Maheswaran <vithu.maheswaran@gmail.com>
2023-11-02 13:58:33 -0400

    Update myFirstTest.test.js

--- Tutorial7/tests/myFirstTest.test.js
+++ Tutorial7/tests/myFirstTest.test.js
@@ -30,7 +30,7 @@ describe("Number Operations", ()=>{
     test("1 plus 1 should be equal to 2", ()=>{
         let a = 1;
         let b = 1;
-        expect(a + b).toBe(2)
+        expect(a - b).toBe(2)
     })
     
     test("5 plus 6 is not equal to 10",()=>{
